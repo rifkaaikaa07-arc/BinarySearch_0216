7cc12983a48d3c9ff076ff2e6ccf8f594a307bbe
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-14 11:09:39 +0700

    menambahkan for

--- binary.cpp
+++ binary.cpp
@@ -21,4 +21,14 @@ void input()
             cout << "\n[!] jumlah element tidak boleh lebih dari 10. silahkan coba lagi.\n";
         }
     }
+
+    cout << "\n===============================================\n";
+    cout << "             Masukkan Element Array              \n";
+    cout << "===============================================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << i + 1 << " = ";
+        cin >> element[i];
+    }
 }
