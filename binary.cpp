614c2919ebe5f6b8d56698e7c4fdde835912e8b8
rifkaaikaa07-arc <rifkaaikaa07@gmail.com>
2026-04-14 11:12:32 +0700

    menambahkan for pada void display

--- binary.cpp
+++ binary.cpp
@@ -57,4 +57,14 @@ void display()
     cout << "\n===============================================\n";
     cout << "      Data Array Setelah Diurutkan (Asc)        \n";
     cout << "===============================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element [j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
 }
